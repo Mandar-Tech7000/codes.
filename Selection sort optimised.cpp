059b260a2325dcdef1a0b16d91c79b0b5aedd0f1
Mandar-Tech7000 <mandarkumatkar10@gmail.com>
2025-08-01 16:16:05 +0530

    Add files via upload

---
+++ Selection sort optimised.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+void selection_sort(float percentage[], int n) 
+{
+    for (int i = 0; i < n - 1; i++) 
+    {
+        int minIndex = i;
+        int comparisons = 0;
+
+        for (int j = i + 1; j < n; j++) 
+        {
+            comparisons++;
+            if (percentage[j] < percentage[minIndex]) 
+            {
+                minIndex = j;
+            }
+        }
+
+        
+        if (minIndex != i) 
+        {
+            float temp = percentage[i];
+            percentage[i] = percentage[minIndex];
+            percentage[minIndex] = temp;
+        }
+
+        
+        cout << "\nPass " << i + 1 << ": Comparisons = " << comparisons << endl;
+        cout << "Array: ";
+        for (int k = 0; k < n; k++) 
+        {
+            cout << percentage[k] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() 
+{
+    int n;
+    cout << "Enter number of students in first year: ";
+    cin >> n;
+
+    
+    float percentage[n];
+
+    for (int i = 0; i < n; i++) 
+    {
+        cout << "Enter percentage for student " << i + 1 << ": ";
+        cin >> percentage[i];
+    }
+
+    selection_sort(percentage, n);
+
+    cout << "\nPercentages in ascending order:\n";
+    for (int i = 0; i < n; i++) 
+    {
+        cout << percentage[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
